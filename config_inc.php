22de0318f66b5af65fb3fcce677e62b5d4b1a003
nisha126 <nishadotcom126@gmail.com>
2015-09-29 12:36:45 +0530

    I am getting my foot wet

---
+++ config_inc.php
@@ -0,0 +1,18 @@
+<?php
+	//Modification Build
+	/*$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'bbt';
+	
+	$g_database_name = 'bbt';
+	$g_db_username = 'bbt';
+	$g_db_password = 'bbt@bits';*/
+	
+	$g_hostname = '192.168.1.8';
+	$g_db_type = 'mysql';
+	$g_database_name = 'bbt';
+	
+	/*$g_database_name = 'bbt';*/
+	$g_db_username = 'php';
+	$g_db_password = 'php';
+?>
